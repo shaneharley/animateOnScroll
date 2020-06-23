57f3e877d8289722380a4df0aa3196d878d4c243
Shane Harley <shaneharley@gmail.com>
2020-06-22 19:56:37 -0700

    Added readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# animateOnScroll
+
+Building something that when scrolling down for the first time does:
+- Fades in elements in view in a staggered fashion detected by IntersectionObserver
+- Detects whether a section has a dark BG or light and changes global styles accordingly
+- Ensures that on scroll up it doesn't animate again
--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 //it passes in the entry that just passed the threshold
 const animateTextIn = (entry) => {
   //gets every child of the entry
-  //this is weird, because the .children doesn't return an array, I have to turn it into one in order to use forEach with it
   let childrenOfEntry = Array.from(entry.target.children)
   let transitionTime = 0.0
 
@@ -14,13 +13,12 @@ const animateTextIn = (entry) => {
         return
       } else {
         //THIS IS HOW I MAKE IT DO A STAGGERED FADE IN AND SLIDE UP ON FIRST LOAD
-        //here i'm saying that if it hasn't appeared for the first time, I just want to transition the opacity and transform NOT the color
         child.style.transitionDelay = transitionTime + "s"
         child.classList.add("active")
         transitionTime = transitionTime + .3
 
-        //THIS IS HOW I WIPE THOSE STYLES FOR FUTURE LOADS
-        let waitingTime = (childrenOfEntry.length - 1) * (10 * 100)
+        //this is how I calculate how long each item should wait
+        let waitingTime = (childrenOfEntry.length) * (1000)
         //running a timeout where it resets the styles and applies the data attribute of loaded to prevent it loading future times
         setTimeout(() => {
           childrenOfEntry.forEach(child => {
@@ -33,6 +31,7 @@ const animateTextIn = (entry) => {
   })
 }
 
+//function to just switch the mode depending on where we're at on the page
 const toggleDarkMode = (entry) => {
   if (entry.target.dataset.mode == "dark") {
     document.body.dataset.theme = "dark"
@@ -40,6 +39,9 @@ const toggleDarkMode = (entry) => {
     document.body.dataset.theme = "light"
   }
 }
+
+//INTERSECTION OBSERVER
+///////////////////////////////////////////////
 
 //this is creating my observer
 const createObserver = () => {
@@ -73,7 +75,6 @@ const handleIntersect = (entries, observer) => {
   })
 }
 
-//here I'm saying once it's run, create the observer
 window.addEventListener('load', (event) => {
   createObserver()
 })
